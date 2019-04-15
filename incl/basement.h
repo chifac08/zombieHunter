228210d580c3c9f6f596d7492721f99ad1c65873
Fabian Lichtenegger-Lukas <lilufa1993@gmail.com>
2019-04-15 21:31:22 +0200

    For base functions and variables

--- incl/basement.h
+++ incl/basement.h
@@ -1,5 +1,28 @@
+#include <stdio.h>
+
+#define DEFAULT_CONFIG_FILE "/opt/etc/zombieHunter/hunter.cfg"
+#define DEFAULT_LOG_FILE "/tmp/test.log"
+#define DEFAULT_LOG_CACHE 5
+
 typedef enum {
 	false=0,
 	true=1
 } bool;
 
+typedef enum
+{
+    ERROR=0,
+    CRIT=1,
+    WARN=2,
+    INFO=3,
+    DEBUG=4
+} LOG_LEVEL;
+
+typedef struct
+{
+	LOG_LEVEL logLevel;
+	unsigned int listenPort;
+	unsigned int logRotate;
+	char logFile[4048];
+	unsigned int logBuffer;
+} CONFIG;
---
+++ src/basement.c
@@ -0,0 +1,46 @@
+/*
+ * basement.c
+ *
+ * Holds base functions.
+ *
+ *  Created on: Apr 7, 2019
+ *      Author: chifac08
+ */
+#include <stdlib.h>
+#include <string.h>
+#include "basement.h"
+
+/**
+ * @brief Check if list contains element. list must have a fixed size!
+ * @author chifac08
+ * @return true or false
+ */
+bool contains(char** list, char* strTarget)
+{
+	int iListLength = strlen(list);
+	bool bFound = false;
+
+	for(int i = 0; i < iListLength;i++)
+	{
+		if(strcmp(strTarget, *(list+i)) == 0)
+		{
+			bFound = true;
+			break;
+		}
+	}
+
+	return bFound;
+}
+
+
+void removeSpecialChars(char* cpTarget, char** listSpecChars)
+{
+	int iTargetLength = strlen(cpTarget);
+
+	for(int i = 0; i < iTargetLength;i++)
+	{
+		char* cpChar = cpTarget[i];
+		if(contains(&listSpecChars, cpChar))
+			printf("test");
+	}
+}
